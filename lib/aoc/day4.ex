c50e3a0099e2624226910af0284150504abb9fa1
Colin Suckow <csuckow99@gmail.com>
2020-12-05 21:39:20 -0800

    Day 6

--- lib/aoc/day4.ex
+++ lib/aoc/day4.ex
@@ -1,79 +1,79 @@
-# defmodule Day4 do
-#   def run do
-#     contents = elem(File.read("input_files/day4.txt"), 1)
-#     |> String.split("\n\n", trim: true)
-#     |> Enum.map(&String.replace(&1, "\n", " "))
-#     |> Enum.filter(&validate(&1))
-#     |> length
-#   end
+defmodule Day4 do
+  def run do
+    contents = elem(File.read("input_files/day4.txt"), 1)
+    |> String.split("\n\n", trim: true)
+    |> Enum.map(&String.replace(&1, "\n", " "))
+    |> Enum.filter(&validate(&1))
+    |> length
+  end
 
-#   def validate(line) do
-#     values_valid = line
-#     |> String.split(" ", trime: true)
-#     |> Enum.map(fn(v) ->
-#       vs = String.split(v, ":", trim: true)
-#       first = Enum.at(vs, 0)
-#       last = Enum.at(vs, 1)
-#       case first do
-#         "byr" -> last |> Integer.parse(10) |> elem(0) |> check_year(1920, 2002)
-#         "iyr" -> last |> Integer.parse(10) |> elem(0) |> check_year(2010, 2020)
-#         "eyr" -> last |> Integer.parse(10) |> elem(0) |> check_year(2020, 2030)
-#         "hgt" -> check_height(last)
-#         "hcl" -> check_hair_color(last)
-#         "ecl" -> check_eye_color(last)
-#         "pid" -> check_id(last)
-#         _ -> true
-#       end
-#     end)
-#     |> Enum.member?(false) == false
+  def validate(line) do
+    values_valid = line
+    |> String.split(" ", trime: true)
+    |> Enum.map(fn(v) ->
+      vs = String.split(v, ":", trim: true)
+      first = Enum.at(vs, 0)
+      last = Enum.at(vs, 1)
+      case first do
+        "byr" -> last |> Integer.parse(10) |> elem(0) |> check_year(1920, 2002)
+        "iyr" -> last |> Integer.parse(10) |> elem(0) |> check_year(2010, 2020)
+        "eyr" -> last |> Integer.parse(10) |> elem(0) |> check_year(2020, 2030)
+        "hgt" -> check_height(last)
+        "hcl" -> check_hair_color(last)
+        "ecl" -> check_eye_color(last)
+        "pid" -> check_id(last)
+        _ -> true
+      end
+    end)
+    |> Enum.member?(false) == false
 
-#     values_valid and String.contains?(line, "byr") and String.contains?(line, "iyr") and String.contains?(line, "eyr") and String.contains?(line, "hgt") and String.contains?(line, "hcl") and String.contains?(line, "ecl") and String.contains?(line, "pid")
-#   end
+    values_valid and String.contains?(line, "byr") and String.contains?(line, "iyr") and String.contains?(line, "eyr") and String.contains?(line, "hgt") and String.contains?(line, "hcl") and String.contains?(line, "ecl") and String.contains?(line, "pid")
+  end
 
-#   def check_year(num, low, high) do
-#     num >= low and num <= high
-#   end
+  def check_year(num, low, high) do
+    num >= low and num <= high
+  end
 
-#   def check_height(val) do
-#     if Regex.match?(~r/[a-z]+/, val) do
-#       h_range = case Regex.scan(~r/[a-z]+/, val) |> Enum.at(0) |> Enum.at(0) do
-#         "cm" -> 150..193
-#         "in" -> 59..76
-#         _ -> 0..0
-#       end
-#       h = Regex.scan(~r/[0-9]+/, val)
-#       |> Enum.at(0)
-#       |> Enum.at(0)
-#       |> Integer.parse(10)
-#       |> elem(0)
-#       Enum.member?(h_range, h)
-#     else
-#       false
-#     end
-#   end
+  def check_height(val) do
+    if Regex.match?(~r/[a-z]+/, val) do
+      h_range = case Regex.scan(~r/[a-z]+/, val) |> Enum.at(0) |> Enum.at(0) do
+        "cm" -> 150..193
+        "in" -> 59..76
+        _ -> 0..0
+      end
+      h = Regex.scan(~r/[0-9]+/, val)
+      |> Enum.at(0)
+      |> Enum.at(0)
+      |> Integer.parse(10)
+      |> elem(0)
+      Enum.member?(h_range, h)
+    else
+      false
+    end
+  end
 
-#   def check_hair_color(val) do
-#     Regex.match?(~r/#[0-9a-f]{6}/, val) and val |> String.graphemes() |> length() < 8
-#   end
+  def check_hair_color(val) do
+    Regex.match?(~r/#[0-9a-f]{6}/, val) and val |> String.graphemes() |> length() < 8
+  end
 
-#   def check_eye_color(val) do
-#     case val do
-#       "amb" -> true
-#       "blu" -> true
-#       "brn" -> true
-#       "gry" -> true
-#       "grn" -> true
-#       "hzl" -> true
-#       "oth" -> true
-#       _ -> false
-#     end
-#   end
+  def check_eye_color(val) do
+    case val do
+      "amb" -> true
+      "blu" -> true
+      "brn" -> true
+      "gry" -> true
+      "grn" -> true
+      "hzl" -> true
+      "oth" -> true
+      _ -> false
+    end
+  end
 
-#   def check_id(val) do
-#     is_num = case Integer.parse(val) do
-#       {_, _} -> true
-#       _ -> false
-#     end
-#     is_num and val |> String.graphemes() |> length == 9
-#   end
-# end
+  def check_id(val) do
+    is_num = case Integer.parse(val) do
+      {_, _} -> true
+      _ -> false
+    end
+    is_num and val |> String.graphemes() |> length == 9
+  end
+end
---
+++ lib/aoc/day6.ex
@@ -0,0 +1,33 @@
+defmodule Day6 do
+  def run do
+    elem(File.read("input_files/day6.txt"), 1)
+    |> String.split("\n\n", trim: true)
+    |> Enum.map(fn(line) -> line |> String.graphemes |> Enum.filter(fn(c) -> c != "\n" end) end)
+    |> Enum.map(fn(line) -> line |> Enum.uniq |> length end)
+    |> Enum.sum()
+  end
+
+  def run2 do
+    elem(File.read("input_files/day6.txt"), 1)
+    |> String.split("\n\n", trim: true)
+    |> Enum.map(fn(line) -> line |> String.split("\n", trime: true) end)
+    |> Enum.map(fn(group) ->
+      letters = for n <- ?a..?z, do: << n :: utf8 >>
+      letters
+      |> Enum.map(fn(letter) -> all_yes(letter, group) end)
+      |> Enum.filter(fn(x) -> x == true end)
+      |> length
+    end)
+  |> Enum.sum
+  end
+
+
+  def all_yes(char, [] = group) do
+    false
+  end
+
+  def all_yes(char, group) do
+    group
+    |> Enum.all?(fn(p) -> p |> String.graphemes |> Enum.member?(char) end)
+  end
+end
