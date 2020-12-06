6f2d6b83269ead0ddd4be85e6c4143f21b365ad0
Colin Suckow <csuckow99@gmail.com>
2020-12-05 21:44:09 -0800

    Fix 'trime' typo in day6

--- lib/aoc/day6.ex
+++ lib/aoc/day6.ex
@@ -10,7 +10,7 @@ defmodule Day6 do
   def run2 do
     elem(File.read("input_files/day6.txt"), 1)
     |> String.split("\n\n", trim: true)
-    |> Enum.map(fn(line) -> line |> String.split("\n", trime: true) end)
+    |> Enum.map(fn(line) -> line |> String.split("\n", trim: true) end)
     |> Enum.map(fn(group) ->
       letters = for n <- ?a..?z, do: << n :: utf8 >>
       letters
